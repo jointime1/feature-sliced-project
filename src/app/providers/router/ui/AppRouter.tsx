6667105eda2e3916f10d9aaffdcffd5aae1b527b
Jointime <jointime.alex@gmail.com>
2023-02-19 13:26:18 +0300

    fix: route config

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,11 +2,15 @@ import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
 import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+            {Object.values(routeConfig).map(({element, path})=> (
+                <Route key={path} path={path} element={element}/>
+            ))}
           <Route path='/' element={<MainPage />} />
           <Route path='/about' element={<AboutPage/>} />
         </Routes>
