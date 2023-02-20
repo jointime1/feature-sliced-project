9b7bf14f7227c5262a5ef18e07cdc72b778e8468
Jointime <jointime.alex@gmail.com>
2023-02-20 14:47:08 +0300

    feat: hotreload

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -16,6 +16,7 @@ export function buildPlugins ({paths, isDev} : BuildOptions): WebpackPluginInsta
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(true),
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
