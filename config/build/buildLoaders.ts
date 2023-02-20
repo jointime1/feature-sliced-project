5e6c152849a14d28da2ee199f68824e036a9bc87
Jointime <jointime.alex@gmail.com>
2023-02-20 16:44:14 +0300

    feat: babel plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,24 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
       use: ['@svgr/webpack'],
     }
 
+    const babelLoader =  {
+      test: /\.(js|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env'],
+          "plugins": [
+            ["i18next-extract", {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true
+            }],
+            // […] your other plugins […]
+          ]
+        }
+      }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -42,6 +60,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
