955de026b735f205701a912bfa52319225a758e8
Jointime <jointime.alex@gmail.com>
2023-02-21 17:34:08 +0300

    test: first unit

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,20 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first parametr', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
+    test('with all args (mods)', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+});
