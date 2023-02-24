2a35e42c3fcc1ec27039016e3052a80ef871f248
Jointime <jointime.alex@gmail.com>
2023-02-24 20:58:28 +0300

    feat: visul-screen-report yaml fix

--- src/widgets/SideBar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/SideBar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { ComponentRender } from 'shared/lib/tests/ComponentRender/ComponentRender';
+
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('with text test', () => {
-        renderWithTranslation(<Sidebar />);
+        ComponentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        ComponentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
